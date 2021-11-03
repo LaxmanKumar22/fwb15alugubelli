1a7a2eab364b24f929a255b9a7a6a25af2502824
LaxmanKumar22 <S545015@NWMISSOURI.EDU>
2021-11-03 02:16:20 -0500

    final push

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var kiteRouter = require('./routes/kite');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -20,7 +22,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/kite', kiteRouter);
 app.use('/users', usersRouter);
+app.use('/selector', selectorRouter);
+app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
